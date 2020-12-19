efbc87ce8981021ae657c0ed7fd889ae17ab641e
sorre97 <alessandro.sorrentino@mail.polimi.it>
2020-12-19 19:17:03 +0100

    Main: test reading values with polling

--- miosix-kernel/main.cpp
+++ miosix-kernel/main.cpp
@@ -1,18 +1,20 @@
 
 #include <cstdio>
-#include <miosix.h>
+#include "miosix.h"
 #include "EMGsensor.h"
 
 using namespace std;
 using namespace miosix;
 
 int main()
-{
+{ 
     EMGsensor emgSensor(EMGsensormod::POLLING);
     uint16_t emgVal = 0;
+
     while(1)
     {
-        emgVal = emgSensor.readValue();
+        emgVal = emgSensor.readPolling();
         printf("ADC value: %d\n", emgVal);
+        miosix::Thread::sleep(500);
     }
 }
