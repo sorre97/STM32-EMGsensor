0839eda1d64e633d78de81906e303f3701452559
sorre97 <alessandro.sorrentino@mail.polimi.it>
2020-12-19 19:16:12 +0100

    EMGsensor: implemented polling to read values

--- miosix-kernel/EMGsensor.cpp
+++ miosix-kernel/EMGsensor.cpp
@@ -1,9 +1,10 @@
 #include "EMGsensor.h"
+#include "stm32f401xe.h"
 
 /**
  * EMGsensor()
  * Constructor initializes the peripherals in POLLING mode using default pins
- * Pins: Vin(analog) -> PA0 (ADC1_0)
+ * Redirects to secondary constructor
  * Mode: POLLING
  */
 EMGsensor::EMGsensor() : EMGsensor::EMGsensor(EMGsensormod::POLLING) {}
@@ -12,12 +13,15 @@ EMGsensor::EMGsensor() : EMGsensor::EMGsensor(EMGsensormod::POLLING) {}
  * EMGsensor(Mode mode)
  * Constructor initializes the peripherals in selected mode using default pins if not selected
  * Input: Mode mode. Selects operative mode of the sensor
- * Pins: 
+ * Pins: Vin(analog) -> PA0 (ADC1 channel 0)
  * Mode: POLLING, INTERRUPT, DMA
  */
 EMGsensor::EMGsensor(EMGsensormod::Mode mode)
 {
-    if (mode == EMGsensormod::POLLING)  // POLLING
+    initADC();                         // enables ADC
+    GPIOA->MODER |= GPIO_MODER_MODER0; // PA0 set as analog
+
+    if (mode == EMGsensormod::POLLING) // POLLING
     {
         EMGsensor::configurePolling();
     }
@@ -34,16 +38,41 @@ EMGsensor::EMGsensor(EMGsensormod::Mode mode)
 EMGsensor::~EMGsensor() {}
 
 /* Sensor configuration */
-/* TODO */
+/**
+ * initADC()
+ * Enables ADC clock, sets cycles of conversion, turns ADC power on, selects channel
+ * Access: private
+ */
+void EMGsensor::initADC()
+{
+    RCC->APB2ENR |= RCC_APB2ENR_ADC1EN; // enabling ADC clock
+    ADC1->SQR3 &= ~(ADC_SQR3_SQ1);      // channel 0 select
+    ADC1->SQR1 &= ~(ADC_SQR1_L);        // single conversion
+    ADC1->SMPR1 |= ADC_SMPR2_SMP0;      // 480 cycles for channel 0
+    ADC1->CR2 |= (ADC_CR2_ADON);        // turning ADC on
+}
+
+/**
+ * configurePolling()
+ * Configures ADC for polling
+ * Access: private
+ */
 void EMGsensor::configurePolling()
 {
+    // no need for further configurations
+    // left for possible future extension
     return;
 }
 
-/* TODO */
+/**
+ * configureInterrupt()
+ * Configures ADC for interrupt
+ * Access: private
+ */
 void EMGsensor::configureInterrupt()
 {
-    return;
+    ADC1->CR1 |= ADC_CR1_EOCIE; // Enabling interrupt at EOC
+
 }
 
 /* TODO */
@@ -55,11 +84,22 @@ void EMGsensor::configureDMA()
 /* Sensor reading */
 /**
  * uint16_t readValue()
- * This function reads converted ADC value in the register and returns it
+ * This function reads converted ADC value in the data register and returns it
  * Access: public
  * Return type: uint16_t
  */
-uint16_t EMGsensor::readValue()
+uint16_t EMGsensor::readPolling()
 {
-    return 0;
+    ADC1->CR2 |= ADC_CR2_SWSTART;
+    while (!(ADC1->SR & ADC_SR_EOC))
+        ;
+    return ADC1->DR;
+}
+
+/* TODO */
+uint16_t EMGsensor::getValue()
+{
+    uint16_t emgValue;
+    EMGsensor::emgValues.get(emgValue);
+    return emgValue;
 }
--- miosix-kernel/EMGsensor.h
+++ miosix-kernel/EMGsensor.h
@@ -2,18 +2,20 @@
 #define EMGsensor_H
 
 #include <miosix.h>
+#include <queue.h>
 
 using namespace std;
 
 namespace EMGsensormod
 {
     // Sensor mode
-    enum Mode { 
+    enum Mode
+    {
         POLLING = 0,
         INTERRUPT,
-        DMA 
+        DMA
     };
-}
+} // namespace EMGsensormod
 
 class EMGsensor
 {
@@ -24,13 +26,19 @@ public:
     EMGsensor(EMGsensormod::Mode mode);
     /* Distructor */
     ~EMGsensor();
-    /* Read ADC value */
-    uint16_t readValue();
+    /* Read ADC1 value from data register using polling */
+    uint16_t readPolling();
+    /* Get value from synch queue */
+    uint16_t getValue();
 
 private:
     // Variables
+    /* List of emg sensor values */
+    miosix::Queue<uint16_t, 20> emgValues;
 
     // Functions
+    /* Enables ADC */
+    void initADC();
     /* Configure EMG sensor in polling mode */
     void configurePolling();
     /* Configure EMG sensor in interrupt mode */
@@ -41,7 +49,7 @@ private:
     EMGsensor &operator=(const EMGsensor &other) = delete;
     /* Copy constructor */
     EMGsensor(const EMGsensor &other) = delete;
-    
+
 }; // End of EMGsensor class
 
 #endif // End of EMGsensor_H
