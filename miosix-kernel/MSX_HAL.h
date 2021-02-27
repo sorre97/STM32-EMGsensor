de29fa9e1f1856fcc39b14e0b46c65327a1a6bf9
sorre97 <alessandro.sorrentino@mail.polimi.it>
2021-02-28 00:44:17 +0100

    MSX_HAL: fixed bug on register value assignment

--- miosix-kernel/MSX_HAL.h
+++ miosix-kernel/MSX_HAL.h
@@ -13,6 +13,7 @@
 #define __MSX_HAL_MASK_CHECK(__REGISTER__, __MASK__) ((__REGISTER__) & (__MASK__))
 
 /* Register Modifiers */
-#define __MSX_HAL_REG_CLEAR(__REGISTER__) (__REGISTER__ = 0);
+#define __MSX_HAL_REG_CLEAR(__REGISTER__) ((__REGISTER__) = 0);
+#define __MSX_HAL_REG_SET(__REGISTER__, __VALUE__) ((__REGISTER__) = (__VALUE__));
 
 #endif
